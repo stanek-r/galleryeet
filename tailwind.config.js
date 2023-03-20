4e6e1e2a7d6a4b7b2c99cbca0b55a4389ab236e5
Radim Staněk <stanek@gtomy.com>
2023-03-20 19:12:45 +0100

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,php,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["corporate"]
+  }
+}
