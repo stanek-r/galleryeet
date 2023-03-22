5048d40787429d81ddbb4d4792b5ef44f827a4c3
Radim Staněk <stanek@gtomy.com>
2023-03-22 16:19:34 +0100

    Add maintenance and not found page

--- src/connect.php
+++ src/connect.php
@@ -2,13 +2,14 @@
 $servername = "localhost";
 $username = "username";
 $password = "password";
+$database = "database";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    header("Location: https://galleryeet.net/maintenance");
+    exit();
 }
-echo "Connected successfully";
 ?>
