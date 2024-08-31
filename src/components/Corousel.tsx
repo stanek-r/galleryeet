01d676b7c5e0ca4429df56778270b13f30cc5105
Radim Staněk <gtomyasek@gmail.com>
2024-08-31 16:31:17 +0200

    Add carousel interval

--- src/components/Corousel.tsx
+++ src/components/Corousel.tsx
@@ -1,35 +1,34 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { useEffect, useState } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 export function Corousel() {
+  const [item, setItem] = useState<number>(1);
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      setItem((prev) => (prev % 4) + 1);
+    }, 5000);
+    return () => clearInterval(int);
+  }, []);
+
   return (
     <div className="w-full">
       <div className="carousel h-80 w-full">
         <div id="item1" className="carousel-item w-full">
-          <LazyLoadImage src="/banner1.webp" className="w-full object-cover" />
-        </div>
-        <div id="item2" className="carousel-item w-full">
-          <LazyLoadImage src="/banner2.webp" className="w-full object-cover" />
-        </div>
-        <div id="item3" className="carousel-item w-full">
-          <LazyLoadImage src="/banner3.webp" className="w-full object-cover" />
-        </div>
-        <div id="item4" className="carousel-item w-full">
-          <LazyLoadImage src="/banner4.webp" className="w-full object-cover" />
+          <LazyLoadImage src={`/banner${item}.webp`} className="w-full object-cover" />
         </div>
       </div>
       <div className="flex w-full justify-center gap-2 py-2">
-        <a href="#item1" className="btn btn-xs">
-          1
-        </a>
-        <a href="#item2" className="btn btn-xs">
-          2
-        </a>
-        <a href="#item3" className="btn btn-xs">
-          3
-        </a>
-        <a href="#item4" className="btn btn-xs">
-          4
-        </a>
+        {[1, 2, 3, 4].map((i) => (
+          <button
+            type="button"
+            onClick={() => setItem(i)}
+            className={twMerge('btn btn-xs', item == i && 'btn-neutral')}
+          >
+            {i}
+          </button>
+        ))}
       </div>
     </div>
   );
